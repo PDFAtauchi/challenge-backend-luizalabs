d81ec3b821fe9d86226fdf211fccd45acc43a461
Paul Dany Flores Atauchi <FloresAtauchi@gmail.com>
2024-06-11 09:52:03 -0300

    Configure Tests Metrics (#7)
    * refactor: removing preconfig testcontainers for mongodb
    
    * build: add pitest and configure for mutation and coverage tests
    
    * docs: update readme for use of pitest tool

--- src/test/kotlin/com/atauchi/transformerFileService/TestTransformerFileServiceApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.atauchi.transformerFileService
-
-import org.springframework.boot.fromApplication
-import org.springframework.boot.with
-
-fun main(args: Array<String>) {
-    fromApplication<TransformerFileServiceApplication>().with(TestcontainersConfiguration::class).run(*args)
-}
