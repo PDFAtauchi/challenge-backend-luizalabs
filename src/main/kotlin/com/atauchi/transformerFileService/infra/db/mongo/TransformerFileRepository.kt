0c234854051fddb4e7d3a54978032dbd3a036ab6
patauchi <floresatauchi@gmail.com>
2024-06-11 15:54:06 -0300

    feat: add mongodb repository

---
+++ src/main/kotlin/com/atauchi/transformerFileService/infra/db/mongo/TransformerFileRepository.kt
@@ -0,0 +1,5 @@
+package com.atauchi.transformerFileService.infra.db.mongo
+
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface TransformerFileRepository : MongoRepository<FileDocument, String>
